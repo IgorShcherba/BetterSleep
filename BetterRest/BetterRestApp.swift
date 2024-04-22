ff2b0a164fb776b3f4b033123f25e31710a46e70
Igor Shcherba <igor.shcherba@callstack.com>
2024-04-22 13:32:27 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Igor Shcherba on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
